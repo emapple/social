efbea8d1b54dfe06f3f9d6b4d9b043eae3291168
Nikhil Tilak <nikhiltilak1991@gmail.com>
2020-05-04 14:13:26 -0400

    Added weighted avg function to weather tools

--- utils/weather_tools.py
+++ utils/weather_tools.py
@@ -1,30 +1,16 @@
-# This code extracts weather data from the NOAA ASOS system :
-# https://www.weather.gov/asos/asostech
-
-# All the ASOS data can be accessed via ftp from their server :
-# ftp://ftp.ncdc.noaa.gov/pub/data/noaa
-
-# Within this there are folders for each years data named 'yyyy'.
-
-# Inside each year there are gzipped files for each weather station in the
-# network.
-
-# The name of each file follows a pattern given by: AAAAAA-BBBBBB-YYYY.gz.
-# For example,
-# 723150-03812-2006 corresponds with USAF number 723150 and WBAN number 03812.
-
-# USAF and WBAN are identifiers for each station and their information is
-# available in the **ASOS_stations.txt** file
-
-# YYYY stands for year
-
-
-# Import statements
-
 import pandas as pd
 import numpy as np
-import re
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+    
+DATA_DIR = os.path.abspath('../state_data')
 
+
+
+df_stations_US = pd.read_csv(f"{DATA_DIR}/stations_TZ.csv")
 
 def generate_stationID_dict():
     """Generate a dict of state name : stationID key/value pairs
@@ -37,114 +23,230 @@ def generate_stationID_dict():
     ST = State for US stations
     """
 
-    filepath_stations = (
-        "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.txt")
-    cols_to_use = ['USAF', 'WBAN', 'CTRY', 'ST']
-    df_stations = pd.read_fwf(
-        filepath_stations, skiprows=20, header=0, usecols=cols_to_use)
-    df_stations_US = df_stations[df_stations.CTRY == 'US'].copy()
-    df_stations_US.WBAN = df_stations_US.WBAN.astype('int64')
-    df_stations_US.dropna(inplace=True)
-    df_stations_US = df_stations_US[df_stations_US.WBAN != 99999].copy()
+    cols_to_use = ['ST', 'StID']
+    df_stations = pd.read_csv(f"{DATA_DIR}/stations_TZ.csv", usecols=cols_to_use)
 
-    def my_func(row):
-        return str(row.USAF) + '-' + str(row.WBAN).zfill(5)
-    df_stations_US['stationID'] = df_stations_US.apply(my_func, axis=1)
+    states = pd.unique(df_stations.ST.values)
 
-    states = pd.unique(df_stations_US.ST.values)
     stations_in_state = {}
     for i in range(len(states)):
-        stations_in_state[states[i]] = list(
-            df_stations_US[df_stations_US.ST == states[i]].stationID.values)
+        stations_in_state[states[i]] = list(df_stations[df_stations.ST == states[i]].StID.values)
     return stations_in_state
+
 stations_in_state = generate_stationID_dict()
 
+key_list = df_stations_US.StID.values
+valu_list = df_stations_US.TZone.values 
+TZ_dict = {key_list[i] : valu_list[i] for i in range(len(key_list))} # dictionary containing {Station_ID: Time-Zone}
 
-def df_from_file(stationID, year):
-    """Takes stationID and year and outputs a dataframe with Temperature
+def process_file(f):
+    
+    StID = f[-20:-8]
+    good = ['1','5', 'A', 'C', 'U'] #temperature quality codes which indicate good data
+    df = pd.read_csv(f, names = ['data'], compression='gzip', header=None)
+    df_no_summary = df[df.data.str.contains('FM-12')].copy()
+    if len(df_no_summary) != 0: #check for files not containing FM-12
+        df_no_summary.reset_index(drop = True, inplace = True)
+        df_no_summary['Date'] = pd.to_datetime(df_no_summary['data'].str[15:27], utc = True)
+        df_no_summary['Date'] = df_no_summary.apply(lambda x: x.Date.astimezone(tz=TZ_dict[StID]), axis = 1) #converting to local timezone
+        df_no_summary['T_C'] = df_no_summary.apply(lambda row : int(row.data[87:92])/10 if (row.data[92] in good) else np.nan, axis = 1)    
+        df_no_summary.drop(columns = ['data'], inplace = True) 
+        return df_no_summary.resample('D', on = 'Date').mean()
 
-    Note that some tempertaure readings are missing in general.
-    """
-
-    url_common = "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/"
-    url_ftp = url_common + str(year) + "/" + \
-        stationID + "-" + str(year) + ".gz"
-
-    WBAN = re.findall(r'(.*)-(.*)', stationID)
-    WBAN = WBAN[0][1]
-
-    #df = df_tmp[0].str.extractall(r'24 HR AVG TEMP \(F\):(\s)?(?P<Temperature>\d\d)')
-
-    def extract_Temp(row):
-        result = re.findall(
-            r'24 HR AVG TEMP \(F\):(\s)?(?P<Temperature>\d\d)', row)
-        if len(result) == 0:
-            result = [(np.nan, np.nan)]
-        return result[-1][1]
-
-    def extract_date(row):
-        date = [np.nan, np.nan, np.nan]
-        date = re.findall(WBAN + r'(\d\d\d\d)(\d\d)(\d\d)', row)
-        if date != []:
-            date = date[0][0] + date[0][1] + date[0][2]
-        return date
-    try:
-        df = pd.read_csv(url_ftp, compression='gzip', header=None)
-        df['TEMPERATURE' + stationID] = df[0].apply(extract_Temp)
-        df['Date'] = df[0].apply(extract_date)
-        df.dropna(inplace=True)
-        df.drop(columns=[0], inplace=True)
-        df['Date'] = pd.to_datetime(df['Date'])
-        df['TEMPERATURE' + stationID] = df['TEMPERATURE' +
-                                           stationID].astype('int64')
-        df.reset_index(inplace=True)
-        df.drop(columns=['index'], inplace=True)
-        return df
-    except (pd.errors.ParserError, OSError, ValueError) as e:  # URLError:
-        #print('No good')
-        print(f'{e} with file: ' + url_ftp)
-        Date = pd.date_range(str(year) + '-01-01', str(year) + '-12-31')
-        df = pd.DataFrame(data=Date, columns=['Date'])
-        return df
-
+    else: #skip file. Does not contain FM-12
+        return 0
 
 def all_stations_for_state_year(state, year):
     """Reads data from all stations in a given state and year
 
-    Returns a dataframe with all the dates in that year temperatures. 
-    The last column contains mean temperatures.
+    Returns: DataFrame with Date index and separate columns for each station-ID
+    
     """
 
     df_list = []
-    Date = pd.date_range(str(year) + '-01-01', str(year) + '-12-31')
-    df = pd.DataFrame(data=Date, columns=['Date'])
-    for stID in stations_in_state[state]:
-        # print(stID)
-        xx = df_from_file(stID, year)
-        # put code here which gets rid of leap years?
-        df_list.append(xx)
+
+    files_processed = 0
+    not_found = 0
+    files_wo_FM12 = 0
+
+    tot_files = len(stations_in_state[state])
+    print(f"Files to process: {tot_files}")
+
+    for StID in stations_in_state[state]:
+        f = "/Volumes/Nikhil hdd/Weather Data/" + year + "/" + StID + "-" + year + ".gz"
+        try:
+            xx = process_file(f)
+            if type(xx) == pd.core.frame.DataFrame :
+                print(f"file: {f}")
+                xx.rename(columns = {'T_C':StID}, inplace = True)
+                df_list.append(xx)
+                files_processed += 1
+            else:
+                files_wo_FM12 += 1
+                
+
+        except (FileNotFoundError, EOFError, pd.errors.ParserError) as e:
+            not_found += 1
+    
+    print(f"files processed: {files_processed}")
+    print(f"files without FM-12: {files_wo_FM12}")
+    print(f"files not_found: {not_found}")  
+
+    StID_0 = stations_in_state[state][0] #representative station ID from the state
+
+    Date = pd.date_range(str(year) + '-01-01', str(year) + '-12-31', tz = TZ_dict[StID_0])
+    df = pd.DataFrame(data=Date, columns=['Date'])     
+
     for frame in df_list:
         df = df.merge(frame, on='Date', how='left')
     df.set_index('Date', inplace=True)
-    df['MEAN_T'] = df.mean(axis=1)
-    df['M/D'] = df.index.strftime('%m/%d')
-    return df
+    df.to_csv("/Volumes/Nikhil hdd/Weather Data/" + f"{state}" +f"_{year}" + ".csv")
+    
+
+def all_states_files(year):
+
+    """Generate temeprature files for all the states for given year
+
+    returns files named ST_YYYY.csv """
+
+    states = pd.unique(df_stations_US.ST.values)
+
+    for state in states:
+        
+        try:
+            path = "/Volumes/Nikhil hdd/Weather Data/" + f"{state}" + "_" + year + ".csv"
+            if not os.path.isfile(path):
+                print(state)
+                all_stations_for_state_year(state,year)
+        except ValueError:
+            print(f'Some issue with {state}')
+
+def states_timeseries():
+
+    """ concatenate files for a state from 2015-2019
+
+    generates time series for each station in the state """
+
+    states = ['AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'FL', 'GA', 'IA', 'ID',
+ 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN','MO', 'MS', 'MT', 'NC',
+ 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH','OK', 'OR', 'PA', 'RI', 'SC',
+ 'SD', 'TN', 'TX', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY']
+
+    
+    years = range(2015,2020,1)
+    
+
+    
+
+    for state in states:
+        print(f'Processing {state}')
+
+        df_list = []
+
+        for year in years:
+
+            try:
+
+                filepath = f"/Volumes/Nikhil hdd/Weather Data/{year}_processed/{state}_{year}.csv"
+                df = pd.read_csv(filepath)
+                df_list.append(df)    
+
+            except FileNotFoundError as e:
+                print(e)
+
+        df = pd.concat(df_list, axis = 0)
+        # df = df_list[0]
+        df['MEAN_T'] = df.mean(axis = 1)*(9/5) + 32
+        
+        try:
+            df.rename(columns = {'Date': 'DATE'}, inplace = True)
+            df.set_index('DATE', inplace = True)
+        except KeyError as e:
+            print(e)
+            df.rename(index = {'Date': 'DATE'}, inplace = True)
+            
+        # df['MEAN_T'].to_csv(f"/Volumes/Nikhil hdd/Weather Data/state_data/{state}_2015-2019_raw.csv") 
+        df['MEAN_T'].to_csv(f"/Volumes/Nikhil hdd/Weather Data/state_data/{state}_2015-2019_raw.csv") 
+        
 
 
-def state_years(state, start_year, end_year):
-    """Mean temperature data for all years in range
 
 
-    Year range is (start_year, end_year) inclusive
 
-    Returns a single DataFrame
+file_st = f"{DATA_DIR}" + "/" + "states_info.csv"
+df_states = pd.read_csv(file_st, index_col = 0, parse_dates = ['LD'])
+regions = df_states.columns[2:-3]
+states = df_states.index.values
+states_in_region = {}
+
+for region in regions:
+    state_list = []
+    for state in states:
+        if not pd.isna(df_states.loc[state][region]):# state is in the region
+            state_list.append(state)
+    states_in_region[region] = state_list
+
+
+def weighted_temperature(region):
+    
+    """Takes all the states in the region and generates a population weighted temperature for the region.
+    
+    The output frame contains 
+    index : DATE
+    columns : T_(statename)
+              T_WA
     """
-    years = range(start_year, end_year + 1, 1)
+    
+    states = states_in_region[region]
+    print(f"states to process: {states}")
+    
     df_list = []
-    for year in years:
-        xx = all_stations_for_state_year(state, year)
-        df_list.append(xx[['MEAN_T']])
+    for state in states:
+        filepath_15to19 = f"{DATA_DIR}" + "/" + state + "_2015-2019_raw.csv"
+        filepath_2020 = f"{DATA_DIR}" + "/" + state + "_2020_raw.csv"
+        T_till_2019 = pd.read_csv(filepath_15to19, index_col = 'DATE', parse_dates = True) #2015-2019 data
+        T_2020 = pd.read_csv(filepath_2020, index_col = 'DATE', parse_dates = True) #2020 data
+
+        T_till_2019.index = T_till_2019.index.map(lambda x: pd.to_datetime(x.strftime('%Y-%m-%d')))
+        T_2020.index = T_2020.index.map(lambda x: pd.to_datetime(x.strftime('%Y-%m-%d')))
+
+        T_all = T_till_2019.append(T_2020) # 2015-2020 data
+        T_all.dropna(inplace = True) #dropping some missing entries
+        T_all.rename(columns = {'MEAN_T': f'{state}'}, inplace = True)
+        df_list.append(T_all)
+        print(f'processed: {state}')
+   
+    # df = pd.concat(df_list, axis = 1)
+
     df = df_list[0]
+
     for frame in df_list[1:]:
-        df = df.append(frame)
+        print(f"merging {frame}" )
+        df = df.merge(frame, left_index=True, right_index = True, how='outer')
+    
+    def weighted_average(row):
+        """ Calculates Popoulation weighted Average of Temperature. 
+        
+        Missing Temperature values ignored while calculating weights"""
+        
+        wm = 0 # weighted mean
+        tot_pop = 0 #Population of states with available Temperature data in a particular row
+        
+        for col in row.index:
+            if not pd.isnull(row[col]): 
+                wm += row[col]*df_states.loc[col].Population
+                tot_pop += df_states.loc[col].Population
+        return wm/tot_pop
+
+    print("now applying weighted average.")
+
+    df['T_WA'] = df.apply(weighted_average, axis = 1)
+    
+    df.to_csv(f'{DATA_DIR}' + "/" + f'Regional_{region}_2015-2020.csv') # stores it in a file
     return df
+
+for region in ['NW']:
+    weighted_temperature(region)
+
+
+
